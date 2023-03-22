6cf71f19754dc1f8e3575d2250fab7484e8cdaa7
jasmica140 <98131692+jasmica140@users.noreply.github.com>
2023-03-22 13:41:43 +0100

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by Jasmine Micallef on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
